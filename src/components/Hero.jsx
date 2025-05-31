78a91962ebdc7857ddd97b72ea63f85c73bd716e
modernstacker <YOUR_NO_REPLY_EMAIL@users.noreply.github.com>
2025-05-30 22:59:38 -0400

    last fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -70,23 +70,6 @@ const Hero = () => {
           </button>
         </div>
       </div>
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
-        <a href="#about">
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-hover-active flex justify-center items-start p-2">
-            <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className="w-3 h-3 rounded-full bg-hover-active mb-1"
-            />
-          </div>
-        </a>
-      </div>
     </section>
   );
 };
